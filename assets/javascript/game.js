bef27a1cf43ab7e4b5fc74bf89035f3133d50e27
WD$ <wyntonmdavis@gmail.com>
2019-08-08 10:18:46 -0400

    Initializing random total score goal via html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,12 @@
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
-var totalNumberGoal = "";
-var randomNumbers = "";
+
+
+var rubyNumber = " ";
+var diamondNumber = " ";
+var goldNumber = " ";
+var emeraldNumber = " ";
 
 var isTotal =false;
 
@@ -10,5 +14,17 @@ var crystalNumbers =Math.floor(Math.random() * (12) + 1);
 var randomTotal = Math.floor(Math.random() * (101) + 19);
 
 
+
+
 console.log(crystalNumbers);
 console.log(randomTotal);
+
+function startGame() {
+
+    wins = 0;
+    losses = 0;
+
+    console.log($("#rand-num").html(randomTotal))
+}
+
+startGame();
