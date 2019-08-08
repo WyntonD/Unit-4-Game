901d468024e20cfa6cf13f2484013d9739d7c653
WD$ <wyntonmdavis@gmail.com>
2019-08-08 09:48:13 -0400

    testing random variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+var totalNumberGoal = "";
+var randomNumbers = "";
+
+var isTotal =false;
+
+var crystalNumbers =Math.floor(Math.random() * (12) + 1);
+var randomTotal = Math.floor(Math.random() * (101) + 19);
+
+
+console.log(crystalNumbers);
+console.log(randomTotal);
